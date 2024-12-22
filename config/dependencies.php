a307badbb711c40451de60db3b39fcb47fe6fda1
Slava Ur <gitmooncat@gmail.com>
2024-12-22 03:09:11 +0200

    some config

--- config/dependencies.php
+++ config/dependencies.php
@@ -6,16 +6,40 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 use Psr\Log\LoggerInterface;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Formatter\LineFormatter;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-foreach ($_ENV as $key => $value) {
-	putenv("$key=$value");
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
+    foreach ($_ENV as $key => $value) {
+        putenv("$key=$value");
+    }
+    $dotenv->required([
+        'DB_HOST',
+        'DB_DATABASE', 
+        'DB_USERNAME'
+    ])->notEmpty();
+} catch (\Dotenv\Exception\ValidationException $e) {
+    die('Ошибка валидации переменных окружения: ' . $e->getMessage());
+} catch (\Throwable $e) {
+    die('Ошибка загрузки переменных окружения: ' . $e->getMessage());
 }
+
 $container = new Container();
 $container->set(LoggerInterface::class, function () {
 	$logger = new Logger('app');
 	$logger->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
+	$logger->pushHandler(new RotatingFileHandler(
+		__DIR__ . '/../logs/app.log',
+		30,
+		Logger::WARNING
+	));
+	
+	$formatter = new LineFormatter(
+		"[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
+		"Y-m-d H:i:s"
+	);
 	return $logger;
 });
 $container->set('db', function () {
